ee6798f3df1844ebc7533bdc0a4c036ac1fa562a
Alexis Laks <arek.laks@gmail.com>
2020-02-19 13:07:30 +0100

    All set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from Structuring.structure import opord_to_json
 
-path = os.getcwd() + '/Extracting/data/'
-opord_to_json()
+path = os.getcwd() + '/Extracting/data/Correction OPORD 20(AZ)DB.docx'
+opord_to_json(path, save=True)
