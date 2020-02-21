7158725d4c37506ec494ecb6c5005b0af76f2d36
Alexis Laks <arek.laks@gmail.com>
2020-02-21 17:00:25 +0100

    More stuff, connecting the dots

---
+++ Matching/parser.py
@@ -0,0 +1,45 @@
+from PreProcessing.preprocess import filtering
+
+
+def print_words_between(my_list, word1, word2, last=False):
+    string = ""
+    k, j, i1, i2 = 0, 0, 1000000, 1000000
+    for node in my_list:
+        j = j + 1
+        if filtering(word1) in filtering(node):
+            i1 = min(i1, j)
+        if filtering(word2) in filtering(node):
+            i2 = min(i2, j)
+
+    for node in my_list:
+        k = k + 1
+        if last:
+            i2 = 1000000000000
+        if i1 < k < i2:
+            string += node
+    return string
+
+
+def create_dict(which_list, list_stopwords):
+    dict_stopwords = dict.fromkeys(list_stopwords, 0)
+    length = len(list_stopwords)
+    i = 0
+    while i < length - 1:
+        dict_stopwords[list_stopwords[i]] = print_words_between(which_list, list_stopwords[i], list_stopwords[i + 1])
+        i = i + 1
+    dict_stopwords[list_stopwords[length - 1]] = print_words_between(which_list, list_stopwords[length - 2],
+                                                                     list_stopwords[length - 1], True)
+
+    return dict_stopwords
+
+
+def make_dic(parsed_text, annexes, list_of_titles):
+
+    try:
+        dict_of_dicts = dict.fromkeys(annexes, 0)
+        for i in range(len(parsed_text)):
+            dict_of_dicts[annexes[i]] = create_dict(parsed_text[i], list_of_titles)
+    except Exception as e:
+        print(e)
+
+    return dict_of_dicts
--- Structuring/structure.py
+++ Structuring/structure.py
@@ -1,93 +1,72 @@
-import json
+from __future__ import print_function
 import os
-from PreProcessing.preprocess import filtering
-from Extracting.text_extractor import get_l_of_l
-from Matching.levsim import levenshtein
-
-s = ['Exemplaire', 'PC', 'Référence', 'Fuseau horaire utilisé pour l\'ensemble de l\'ordre',
-     'ARTICULATION', 'Situation',
-     'Forces ennemies',
-     'ENI actuel',
-     'Possibilités de l\'ENI',
-     'ENI futur',
-     'Forces amies',
-     'Renforcements et prélèvements',
-     'Evaluation de la situation par le commandement',
-     'Population',
-     'MISSION',
-     'EXECUTION',
-     'Idée de manoeuvre',
-     ]
+import json
+from Matching.segmenter import recursive_items
+from Matching.segmenter import get_annexes
+from Formatting.formatter import get_arbo
+from Extracting.metadata_extractor import get_meta
+from Extracting.text_extractor import get_text
 
 
-def print_words_between(my_list, word1, word2, last=False):
-    string = ""
-    k, j, i1, i2 = 0, 0, 1000000, 1000000
-    for node in my_list:
-        j = j + 1
-        if levenshtein(filtering(word1), filtering(node)) < 0.3:
-            i1 = min(i1, j)
-        if levenshtein(filtering(word2), filtering(node)) < 0.3:
-            i2 = min(i2, j)
-
-    for node in my_list:
-        k = k + 1
-        if last:
-            i2 = 1000000000000
-        if i1 < k < i2:
-            string += node
-    return string
+def restructure(struct, parsed):
+    for key, value in struct.items():
+        if type(value) is dict:
+            restructure(value, parsed)
+        else:
+            if key in parsed.keys():
+                struct[key] = parsed[key]
+    return struct
 
 
-def create_dict(which_list, list_stopwords):
-    dict_stopwords = dict.fromkeys(list_stopwords, 0)
-    length = len(list_stopwords)
-    i = 0
-    while i < length - 1:
-        dict_stopwords[list_stopwords[i]] = print_words_between(which_list, list_stopwords[i], list_stopwords[i + 1])
-        i = i + 1
-    dict_stopwords[list_stopwords[length - 1]] = print_words_between(which_list, list_stopwords[length - 2],
-                                                                     list_stopwords[length - 1], True)
+def annex_walker(path, struct_path="CANEVAS_STRUCT.json"):
+    # Define your structure in a json and input it as a parameter
+    final_struct = json.load(open(struct_path))
+    list_of_titles = recursive_items(final_struct)
 
-    return dict_stopwords
+    # Initiate mega dic containing everything
+    megadic = {}
+    index = 1
+
+    # Get all OPORD in dir in any format (msoffice or pdf)
+    arbo = get_arbo(path)
+
+    for opord in arbo:
+        # Initiate same key dic to be completed every time
+        try:
+            dic_of_files = {}
+
+            # Get metadata of opord and assign it
+            meta = get_meta(opord)
+            dic_of_files['Title'] = meta['Title']
+            dic_of_files['Author(s)'] = meta['Author(s)']
+            dic_of_files['Last Modified By'] = meta['Last Modified By']
+            dic_of_files['Created Date'] = meta['Created Date']
+            dic_of_files['Modified Date'] = meta['Modified Date']
+            dic_of_files['Location'] = opord
+
+            # Extract text from opo 
+            flat_text = get_text(opord)
+
+            # Seperate annexes
+            list_of_lists, annex_titles = get_annexes(flat_text)
+
+            # Create flat dictionnary of opord
+            flat_dic = make_dic(list_of_lists, annex_titles, list_of_titles)
+
+            # Then restructure it from your definiton, respecting hierarchy and assign it to a field of our sub dic
+            dic_of_annexes = {}
+            for annex in flat_dic.keys():
+                final_struct = json.load(open(struct_path))
+                dic_of_annexes[str(annex)] = restructure(final_struct, flat_dic[str(annex)])
+            dic_of_files["Content"] = dic_of_annexes
+
+            # And assign all of this to our megadic, indexed by incremental numbers!
+            megadic[str(index)] = dic_of_files
+            index += 1
+        except Exception as e:
+            print("ERROR::", e, ':', os.path.basename(opord))
+
+    return megadic
 
 
-def to_json(dic, file_name="metadata.json"):
-    js = json.dumps(dic, indent=1)
-
-    # Open new json file if not exist it will create
-    fp = open(os.getcwd() + "/" + str(file_name), 'w')
-
-    # write to json file
-    fp.write(js)
-
-    # close the connection
-    fp.close()
-
-
-def struct_dict(parsed_text, alt_annexes=False, annex_path=None):
-    if alt_annexes:
-        structure_json = json.load(open(annex_path))
-        annex_titles = list(structure_json.keys())
-    else:
-        annex_path = os.getcwd() + '/Extracting/data/structure.json'
-        structure_json = json.load(open(annex_path))
-        annex_titles = list(structure_json.keys())
-    try:
-        dict_of_dicts = dict.fromkeys(annex_titles, 0)
-        for i in range(len(parsed_text) - 1):
-            dict_of_dicts[annex_titles[i]] = create_dict(parsed_text[i], s)
-    except Exception as e:
-        print(e)
-
-    return dict_of_dicts
-
-
-def opord_to_json(path, save=False):
-    parsed_text = get_l_of_l(path)
-    opord_dic = struct_dict(parsed_text)
-
-    if save:
-        to_json(opord_dic, file_name='parsed_opord.json')
-
-    return opord_dic
+annex_walker(os.getcwd())
